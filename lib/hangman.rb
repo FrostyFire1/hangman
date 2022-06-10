9d24e3b7d15c3596f3cb3a976418e33452087914
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-10 20:02:33 +0200

    Add get_words(), choose_word()

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -15,6 +15,22 @@ class Game
     @incorrect_guesses = []
   end
 
+  def choose_word
+    word_list = get_words
+    @word = word_list.sample
+  end
+
+  def get_words
+    file = 'dictionary.txt'
+    raise ArgumentError, "#{file} not found!" unless File.exist?(file)
+    file_content = File.readlines(file)
+    word_list = []
+    file_content.each do |word|
+      word_list.push(word) if word.length.between?(5, 12)
+    end
+    word_list
+  end
+
   def start
     choose_word
     puts 'Good luck!'
